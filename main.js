39ad588ec2a7a002e737e19863817426683a34a7
PROF ROMULO <rjlpena@gmail.com>
2023-10-04 16:27:41 -0300

    botao vaias

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomAplausos(){
    document.querySelector("#som_tecla_aplausos").play(); 
 }
 document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;
+
+function tocaSomVaias(){
+    document.querySelector("#som_tecla_vaias").play(); 
+ }
+ document.querySelector(".tecla_vaias").onclick = tocaSomVaias;

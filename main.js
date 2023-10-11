709ba8ff36a85cc08eb2b42f90b1d9a9f575cee5
PROF ROMULO <rjlpena@gmail.com>
2023-10-11 16:05:27 -0300

    risada

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function tocaSomAplausos(){
-   document.querySelector("#som_tecla_aplausos").play(); 
+   document.querySelector('#som_tecla_aplausos').play(); 
 }
-document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;
+document.querySelector('.tecla_aplausos').onclick = tocaSomAplausos;
 
 function tocaSomVaia(){
-    document.querySelector("#som_tecla_vaia").play(); 
+    document.querySelector('#som_tecla_vaia').play(); 
  }
- document.querySelector(".tecla_vaia").onclick = tocaSomVaia;
+ document.querySelector('.tecla_vaia').onclick = tocaSomVaia;
+
+ function tocaSomRisada(){}{
+   document.querySelector('#som_tecla_risada').play();
+ }
+ document.querySelector('.tecla_risada').onclick = tocaSomRisada;

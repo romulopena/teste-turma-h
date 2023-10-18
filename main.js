a89491cbefad976ff0ebb78a07bb4cd334928a7a
PROF ROMULO <rjlpena@gmail.com>
2023-10-18 19:34:15 +0000

    const

--- main.js
+++ main.js
@@ -3,12 +3,5 @@ function tocaSomAplausos(){
 }
 document.querySelector('.tecla_aplausos').onclick = tocaSomAplausos;
 
-function tocaSomVaia(){
-    document.querySelector('#som_tecla_vaia').play(); 
- }
- document.querySelector('.tecla_vaia').onclick = tocaSomVaia;
-
- function tocaSomRisada(){
-   document.querySelector('#som_tecla_risada').play();
- }
- document.querySelector('.tecla_risada').onclick = tocaSomRisada;
+ const listaDeTeclas = document.querySelectorAll(".tecla");
+ listaDeTeclas[0].onclick = tocaSomAplausos;

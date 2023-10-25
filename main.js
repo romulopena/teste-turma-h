3bc139303a9a3853e5925dde8fce5b618f8c6e61
PROF ROMULO <rjlpena@gmail.com>
2023-10-25 19:27:56 +0000

    while

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function tocaSomAplausos(){
-   document.querySelector('#som_tecla_aplausos').play(); 
+//criei a função tocaSomAplausos e usamos o .play para tocar o som do id
+function tocaSom(idElementoAudio){
+   document.querySelector(idElementoAudio).play(); 
 }
-document.querySelector('.tecla_aplausos').onclick = tocaSomAplausos;
-
+//criei constante listaDeTeclas e busquei a classe coletiva tecla
  const listaDeTeclas = document.querySelectorAll(".tecla");
- listaDeTeclas[0].onclick = tocaSomAplausos;
+//criei a variavel contador
+let contador = 0;
+//criei o laço de repetição while chamando cada botão da lista de teclas pelo contador
+while(contador < listaDeTeclas.length){
+   const efeito = listaDeTeclas[contador].classList[1];
+   const idAudio = "#som_" + efeito;
+   listaDeTeclas[contador].onclick = function(){
+      tocaSom(idAudio);
+   }
+   contador = contador + 1;
+   //console.log(contador);
+}

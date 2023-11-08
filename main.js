6eda0da33d9efdc5c149390d0f45c6974687cd82
PROF ROMULO <rjlpena@gmail.com>
2023-11-08 19:04:38 +0000

    template string e FOR

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ function tocaSom(idElementoAudio){
 }
 //criei constante listaDeTeclas e busquei a classe coletiva tecla
  const listaDeTeclas = document.querySelectorAll(".tecla");
-//criei a variavel contador
-let contador = 0;
 //criei o laço de repetição while chamando cada botão da lista de teclas pelo contador
-while(contador < listaDeTeclas.length){
-   const efeito = listaDeTeclas[contador].classList[1];
-   const idAudio = "#som_" + efeito;
-   listaDeTeclas[contador].onclick = function(){
+//trocado o while pelo for e inserido o contador e a atualização dele
+for(let contador = 0;contador < listaDeTeclas.length;contador = contador + 1){
+   const tecla = listaDeTeclas[contador];
+   const efeito = tecla.classList[1];
+   const idAudio = `#som_${efeito}`;
+   tecla.onclick = function(){
       tocaSom(idAudio);
    }
-   contador = contador + 1;
-   //console.log(contador);
 }
